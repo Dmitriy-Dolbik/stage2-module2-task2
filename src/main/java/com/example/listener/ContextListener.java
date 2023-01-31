b8335c1846060568bb49ffcd98c7a68fc0a87f8a
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-31 14:27:16 +0300

    Add solution

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,18 @@
 package com.example.listener;
 
-public class ContextListener {
-    //write your code here!
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class ContextListener implements ServletContextListener
+{
+    public void contextInitialized(ServletContextEvent e) {
+        ServletContext context = e.getServletContext();
+        context.setAttribute("servletTimeInt", LocalDateTime.now());
+    }
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,63 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.example.Users;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("user") != null)
+        {
+            request.getRequestDispatcher("login.jsp")
+                    .forward(request, response);
+        }
+        else
+        {
+            request.getRequestDispatcher("/user/hello.jsp")
+                    .forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        HttpSession session = request.getSession();
+        String login = (String) session.getAttribute("login");
+        String password = (String) request.getAttribute("password");
+
+        if (login != null && password != null)
+        {
+            Users usersInstance = Users.getInstance();
+            List<String> usersList = usersInstance.getUsers();
+            if (usersList.contains(login))
+            {
+                session.setAttribute("user", login);
+                request.getRequestDispatcher("/user/hello.jsp")
+                        .forward(request, response);
+            }
+            else
+            {
+                request.getRequestDispatcher("/login.jsp")
+                        .forward(request, response);
+            }
+        } else {
+            request.getRequestDispatcher("/login.jsp")
+                    .forward(request, response);
+        }
+    }
 }
