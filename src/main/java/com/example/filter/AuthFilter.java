b9db755b41fbfc7737e3db246683cb06f0743deb
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-31 14:27:50 +0300

    Add solution2

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,45 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(urlPatterns = "/user/*")
+public class AuthFilter implements Filter
+{
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException
+    {
+        //Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException
+    {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        HttpSession session = httpServletRequest.getSession(false);
+
+        if(session == null || session.getAttribute("user") == null) {
+            httpServletRequest.getRequestDispatcher("/login.jsp")
+                    .forward(httpServletRequest, httpServletResponse);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy()
+    {
+        //Filter.super.destroy();
+    }
 }
